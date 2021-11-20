1f5c7725c4890abdc593943a5731c225eb5ccb3f
MC3Rony <92897037+MC3Rony@users.noreply.github.com>
2021-11-20 17:07:55 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(main);
+var contador = 1;
+
+function main() {
+    $('.menu_bar').click(function() {
+        // $('nav').toggle(); 
+        if (contador == 1) {
+            $('nav').animate({
+                left: '0'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+    });
+};
+$(document).ready(function() {
+    var altura = $('.menu').offset().top;
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > altura) {
+            $('.menu').addClass('menu-fixed');
+        } else {
+            $('.menu').removeClass('menu-fixed');
+        }
+    });
+});
